2b664f32e70b4274671befb90ddf4b5c884e87af
seregatheone <sergey20030707@gmail.com>
2022-03-07 17:30:36 +0300

    solution for 12 problem

---
+++ app/src/main/java/com/example/a12/ui/web/WebFragment.kt
@@ -0,0 +1,34 @@
+package com.example.a12.ui.web
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.a12.databinding.FragmentWebBinding
+
+class WebFragment : Fragment() {
+
+    private var _binding: FragmentWebBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentWebBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
